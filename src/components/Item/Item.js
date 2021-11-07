6d7eed3f8a23f4a728c4fbdd129496c2081959d2
Negro <ramirosuarezpaz@gmail.com>
2021-11-07 19:37:45 -0300

    Muestro productos de firebase

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,23 +4,33 @@ import PropTypes from "prop-types";
 import "./Item.css";
 
 const Item = ({ data }) => (
-  <Card class="tarjeta">
-    <Image src={data.set_img_url} wrapped ui={false} />
+  // <Card class="tarjeta">
+  //   <Image src={data.set_img_url} wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>{data.name}</Card.Header>
+  //     <Card.Meta>
+  //       <span className="date">{data.set_num}</span>
+  //     </Card.Meta>
+  //     <Card.Description>{data.set_url}</Card.Description>
+  //   </Card.Content>
+  // </Card>
+    <Card class="tarjeta">
+    <Image src={data.img} wrapped ui={false} />
     <Card.Content>
-      <Card.Header>{data.name}</Card.Header>
+      <Card.Header>{data.title}</Card.Header>
       <Card.Meta>
-        <span className="date">{data.set_num}</span>
+        <span className="date">{data.id}</span>
       </Card.Meta>
-      <Card.Description>{data.set_url}</Card.Description>
+      <Card.Description>Cantidad de piezas: {data.pieces}</Card.Description>
     </Card.Content>
   </Card>
 );
 
-Item.propTypes = {
-  set_img_url: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  set_num: PropTypes.number,
-  set_url: PropTypes.string,
-};
+// Item.propTypes = {
+//   set_img_url: PropTypes.string,
+//   name: PropTypes.string.isRequired,
+//   set_num: PropTypes.number,
+//   set_url: PropTypes.string,
+// };
 
 export default Item;
