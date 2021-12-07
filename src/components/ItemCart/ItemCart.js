748a3138751051f5dd36365eb499f350a48ddeba
Negro <ramirosuarezpaz@gmail.com>
2021-12-06 21:36:17 -0300

    Intento de mejora de carrito

--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -4,19 +4,29 @@ import "./ItemCart.css";
 const ItemCart = ({ data }) => {
   // </div>
   const handlerIncrement = () => {
-    console.log("agregaste");
+    console.log("agregaste", data.quantity);
+    data.quantity = data.quantity+1
   };
 
   const handlerDecrement = () => {
-    console.log("sacaste");
+    console.log("sacaste", data.quantity);
+    data.quantity = data.quantity-1
   };
+
+  const handlerDelete = () => {
+    console.log("eliminaste", data.quantity);
+    data.quantity = data.quantity=0
+  };
+
+
   return (
     <div className="registroTabla">
       <p>{data.name}</p>
       <p>$ {data.price}</p>
       <p>{data.quantity}</p>
       <button onClick={handlerIncrement}>Agregar</button>
-      <button onClick={handlerDecrement}>Quitar</button>
+      <button onClick={handlerDecrement}>Sacar</button>
+      <button onClick={handlerDelete}>Eliminar</button>
     </div>
   );
 };
